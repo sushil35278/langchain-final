d09c6444d273758f8bf702f8f512a65ee8deb4ea
sushil sigdel <sigsushil98@gmail.com>
2023-08-02 18:29:03 +0545

    working chatbot

---
+++ scripts/ingest-data.ts
@@ -0,0 +1,53 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { pinecone } from '@/utils/pinecone-client';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+
+
+const filePath = 'docs/Counseling and lnformation Services.pdf';
+
+export const run = async () => {
+  try {
+    /*load raw docs from the all files in the directory */
+    // const directoryLoader = new DirectoryLoader(filePath, {
+    //   '.pdf': (path) => new PDFLoader(path),
+    // });
+    const loader = new PDFLoader(filePath)
+
+    // const loader = new PDFLoader(filePath);
+    // const rawDocs = await directoryLoader.load();
+    const rawDocs = await loader.load();
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log('split docs', docs);
+
+    console.log('creating vector store...');
+    /*create and store the embeddings in the vectorStore*/
+    const embeddings = new OpenAIEmbeddings();
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: PINECONE_NAME_SPACE,
+      textKey: 'text',
+    });
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest your data');
+  }
+};
+
+(async () => {
+  await run();
+  console.log('ingestion complete');
+})();
---
+++ utils/makechain.ts
@@ -0,0 +1,37 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `あなたは役に立つAIアシスタントです。以下の文脈を利用して、最後の質問に答えてください。
+答えがわからない場合は、わからないと答えてください。答えを作ろうとしないでください。
+質問が文脈に関係ない場合は、文脈に関係する質問にのみ答えるように調整されていることを丁寧に答えてください。
+
+{context}
+
+Question: {question}
+Helpful answer in markdown:`;
+
+export const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 0, // increase temepreature to get more creative answers
+    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+    },
+  );
+  return chain;
+};
